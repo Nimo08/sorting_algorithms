5613ab3a94d4fb79e655219f6748e6d011474357
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-23 13:20:07 -0700

    selection sort

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -17,4 +18,6 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+void selection_sort(int *array, size_t size);
 #endif
