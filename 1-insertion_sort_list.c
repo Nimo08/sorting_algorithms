94ae5bfe6c14338c2df266675fca5f8d5ea8f61b
Nimo08 <nimomohamed800@gmail.com>
2023-08-25 20:03:30 +0300

    Addes swap function

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,12 @@
 #include "sort.h"
+void swap(listint_t *curr, listint_t *sorted);
 /**
  * insertion_sort_list - sorts a doubly linked list of
  * integers in ascending order
  * @list: double pointer
  * Return: nothing
  */
+
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *temp, *sorted;
@@ -19,26 +21,69 @@ void insertion_sort_list(listint_t **list)
 	while (curr != NULL)
 	{
 		temp = curr->next;
+		sorted = curr->prev;
 		if (sorted == NULL || curr->n < sorted->n)
 		{
-			curr->next = sorted;
-			curr->prev = NULL;
 			if (sorted != NULL)
 			{
-				curr->prev = sorted->prev;
-				sorted->prev = curr;
+				/*insert curr before sorted*/
+				swap(curr, sorted);
+				print_list(*list);
+
 			}
 			sorted = curr;
-			while (sorted != NULL && curr->n < sorted->n)
+			if (curr->prev == NULL)
+				*list = curr;
+			sorted = curr->prev;
+			while (sorted != NULL)
 			{
-				if (curr->n > sorted->n)
+				if (curr->n >= sorted->n)
 					break;
-				curr->next = sorted->next
-				sorted = sorted->prev;
+				/*insert curr before sorted*/
+				swap(curr, sorted);
+				if (curr->prev == NULL)
+					*list = curr;
+				print_list(*list);
+				sorted = curr->prev;
 			}
 		}
-		print_list(sorted);
 		curr = temp;
 	}
-	*list = sorted;
+}
+/**
+ * swap - swaps the nodes
+ * @curr:  current node
+ * @sorted: sorted node
+ * Return: nothing
+ */
+void swap(listint_t *curr, listint_t *sorted)
+{
+	/**
+	 * insert curr before sorted
+	 * account for curr->prev, curr->next
+	 * sorted->prev, sorted->next
+	 * curr->next = sorted, sorted->prev = curr
+	 * curr->prev is going to be the original sorted->prev
+	 * curr->next is going to be original sorted
+	 * 7, 8, 2, 4
+	 * temp stores the 4, so curr->next = 4
+	 * sorted->next = temp
+	 * curr->prev = sorted->prev
+	 * temp->prev = curr
+	 */
+	listint_t *temp;
+
+	temp = curr->next;
+	curr->next = sorted;
+	curr->prev = sorted->prev;
+	if (sorted->prev != NULL)
+	{
+		sorted->prev->next = curr;
+	}
+	sorted->prev = curr;
+	sorted->next = temp;
+	if (temp != NULL)
+	{
+		temp->prev = sorted;
+	}
 }
