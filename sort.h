5cb091c569753a7111b6cea20ad7e3420ae63bbf
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-08-28 23:26:54 +0300

    quick sort hoare

--- sort.h
+++ sort.h
@@ -29,4 +29,5 @@ void shell_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
